3989f421306fd8deff294251c5f0339c18f0f57b
oskretc <otorresbaca@gmail.com>
2013-07-08 12:59:02 +0200

    adding the first spi

---
+++ firs.py
@@ -0,0 +1,36 @@
+import RPi.GPIO as GPIO from time import sleep
+ 
+LED_PIN = 25
+ 
+print "Setting up GPIO" GPIO.setmode(GPIO.BCM) GPIO.setup(LED_PIN, 
+GPIO.OUT)
+ 
+def enable_led(should_enable):
+    print "one"
+    if should_enable:
+        GPIO.output(LED_PIN, False)
+    else:
+        GPIO.output(LED_PIN, True)
+        
+ 
+enable_led(False) print "LED is OFF" sleep(2) enable_led(True) print 
+"LED is ON" sleep(2) enable_led(False) print "LED is OFF" sleep(2) 
+enable_led(True) print "LED is ONNN" sleep(2) GPIO.cleanup() import 
+RPi.GPIO as GPIO from time import sleep
+ 
+LED_PIN = 25
+ 
+print "Setting up GPIO" GPIO.setmode(GPIO.BCM) GPIO.setup(LED_PIN, 
+GPIO.OUT)
+ 
+def enable_led(should_enable):
+    print "one"
+    if should_enable:
+        GPIO.output(LED_PIN, False)
+    else:
+        GPIO.output(LED_PIN, True)
+        
+ 
+enable_led(False) print "LED is OFF" sleep(2) enable_led(True) print 
+"LED is ON" sleep(2) enable_led(False) print "LED is OFF" sleep(2) 
+enable_led(True) print "LED is ONNN" sleep(2) GPIO.cleanup()
