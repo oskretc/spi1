0157c35f6daef616d7e0ee34eabbef918b8ebce1
Oscar <otorresbaca@gmail.com>
2013-07-18 17:55:01 +0200

    dtm attempt

--- uart1.py
+++ uart1.py
@@ -1,17 +1,36 @@
 import serial
 import time
+from decimal import *
+
 usbport = '/dev/ttyAMA0'
-ser = serial.Serial(usbport, 9600, timeout=1)
-data=[0x30,0x31]
+ser = serial.Serial(usbport, 19200, timeout=1)
+data=[0x3D,0x33]
 strdata=''.join(chr(e) for e in data)
 ser.write(strdata)
-ser.timeout = 2
-#time.sleep(2)
+ser.timeout = .5
+#time.sleep(.002)
 
-#while True:
-#           response=ser.read()
-#           time.sleep=2
-#           print (" read data: python \n")
-#           print (response)
-  
+
+response=ser.read(2)
 ser.close()
+#time.sleep=2
+
+
+for data in response:
+
+    try:
+        i = ord(str(data))
+    except ValueError:
+        i = 0
+    print i
+
+#print i
+print (" read data: python \n")
+print len(response)
+#print int(response[0])+1
+
+
+
+
+
+  
