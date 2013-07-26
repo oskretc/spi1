3a75d3770d2375fee02cde1b9f705ad647765f19
Oscar <otorresbaca@gmail.com>
2013-07-26 14:30:24 +0200

    working dtm

--- uart1.py
+++ uart1.py
@@ -2,12 +2,19 @@ import serial
 import time
 from decimal import *
 
+
 usbport = '/dev/ttyAMA0'
 ser = serial.Serial(usbport, 19200, timeout=1)
+data=[0x00,0x00]
+strdata=''.join(chr(e) for e in data)
+ser.write(strdata)
+ser.timeout = 0.1
+response=ser.read(2)
+time.sleep=0.1
 data=[0x3D,0x33]
 strdata=''.join(chr(e) for e in data)
 ser.write(strdata)
-ser.timeout = .5
+
 #time.sleep(.002)
 
 
